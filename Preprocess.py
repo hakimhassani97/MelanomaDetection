0ab52542947de1c0b15b0fd365e1029b736c78f2
hakimhassani97 <hakimhassani97@gmail.com>
2020-03-09 22:02:50 +0100

    added removeArtifactYUB

--- Preprocess.py
+++ Preprocess.py
@@ -39,16 +39,41 @@ class Preprocess:
     def removeArtifactRGB(img):
         # median filtre
         imgMedian = cv2.medianBlur(img, 5, 5)
-        # kernek of 11 * 11
-        kernel = np.ones((11, 11), np.uint8)
         # split RGB channels
         imgB, imgG, imgR = cv2.split(imgMedian)
+        # kernel of 11 * 11
+        kernel = np.ones((11, 11), np.uint8)
         # perform morphologic closing on each RGB channel
         imgClosingB = cv2.morphologyEx(imgB, cv2.MORPH_CLOSE, kernel)
         imgClosingG = cv2.morphologyEx(imgG, cv2.MORPH_CLOSE, kernel)
         imgClosingR = cv2.morphologyEx(imgR, cv2.MORPH_CLOSE, kernel)
         # merge the 3 channels
         imgResult = cv2.merge((imgClosingB, imgClosingG, imgClosingR))
+        return imgResult
+
+    '''
+        DR noise removal
+        1) convert BGR to YUV, 2) process, 3) convert YUV to RGB for OTSU
+        apply morphologic close transformation on each channel of YUV image
+        kernel of (11,11) based on hair size
+    '''
+    @staticmethod
+    def removeArtifactYUV(img):
+        # median filtre
+        imgMedian = cv2.medianBlur(img, 5, 5)
+        # split YUV channels
+        img_yuv = cv2.cvtColor(imgMedian, cv2.COLOR_RGB2YUV)
+        imgV, imgU, imgY = cv2.split(img_yuv)
+        # kernel of 11 * 11
+        kernel = np.ones((11, 11), np.uint8)
+        # perform morphologic closing on each RGB channel
+        imgClosingV = cv2.morphologyEx(imgV, cv2.MORPH_CLOSE, kernel)
+        imgClosingU = cv2.morphologyEx(imgU, cv2.MORPH_CLOSE, kernel)
+        imgClosingY = cv2.morphologyEx(imgY, cv2.MORPH_CLOSE, kernel)
+        # merge the 3 channels
+        imgResult = cv2.merge((imgClosingV, imgClosingU, imgClosingY))
+        # back to RGB
+        imgResult = cv2.cvtColor(imgResult, cv2.COLOR_YUV2RGB)
         return imgResult
     
     '''
@@ -59,6 +84,6 @@ class Preprocess:
         # blur the image
         blur = cv2.blur(img,(15,15))
         # apply OTSU threshold
-        imgray = cv2.cvtColor(blur,cv2.COLOR_BGR2GRAY)
+        imgray = cv2.cvtColor(blur,cv2.COLOR_RGB2GRAY)
         ret, thresh = cv2.threshold(imgray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
         return ret, thresh
