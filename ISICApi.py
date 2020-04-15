a8e75a5f6f400c77c01863fe766f29e578833a37
hakim hassani <30449764+hakimhassani97@users.noreply.github.com>
2020-04-15 21:49:08 +0100

    Revert "Mohamed"

--- ISICApi.py
+++ ISICApi.py
@@ -1,7 +1,6 @@
 import requests
 import os
 
-
 class ISICApi(object):
     def __init__(self, hostname='https://isic-archive.com', username=None, password=None):
         self.baseUrl = f'{hostname}/api/v1'
@@ -14,17 +13,14 @@ class ISICApi(object):
     '''
         appends the base url to the start of the endpoint
     '''
-
     def _makeUrl(self, endpoint):
         return f'{self.baseUrl}/{endpoint}'
 
     '''
         login to the ISIC API
     '''
-
     def _login(self, username, password):
-        authResponse = requests.get(self._makeUrl(
-            'user/authentication'), auth=(username, password))
+        authResponse = requests.get(self._makeUrl('user/authentication'), auth=(username, password))
         # error while loggin in
         if not authResponse.ok:
             raise Exception(f'Login error: {authResponse.json()["message"]}')
@@ -57,16 +53,14 @@ class ISICApi(object):
     '''
         POST the API
     '''
-
     def post(self, endpoint, params=None):
         url = self._makeUrl(endpoint)
         headers = {'Girder-Token': self.authToken} if self.authToken else None
         return requests.post(url, headers=headers, data=params)
-
+    
     '''
         downloads an image from the API
     '''
-
     def downloadSegmentationMask(self, imageId, imgName, dst):
         if not os.path.exists(dst):
             os.makedirs(dst)
@@ -74,11 +68,10 @@ class ISICApi(object):
         print('Downloading %s images' % len(segmentationList))
         for segmentation in segmentationList:
             print(segmentation['_id'])
-            imageFileResp = self.get(
-                'segmentation/%s/mask' % segmentation['_id'])
+            imageFileResp = self.get('segmentation/%s/mask' % segmentation['_id'])
             imageFileResp.raise_for_status()
             imageFileOutputPath = os.path.join(dst, imgName+'.bmp')
             with open(imageFileOutputPath, 'wb') as imageFileOutputStream:
                 for chunk in imageFileResp:
                     imageFileOutputStream.write(chunk)
-            break
+            break;
--- Preprocess.py
+++ Preprocess.py
@@ -1,7 +1,5 @@
-
 import cv2
 import numpy as np
-
 
 class Preprocess:
 
@@ -12,10 +10,10 @@ class Preprocess:
     @staticmethod
     def removeArtifact(img):
         # perform closing to remove hair
-        kernel = np.ones((15, 15), np.uint8)
-        closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel, iterations=2)
+        kernel = np.ones((15,15),np.uint8)
+        closing = cv2.morphologyEx(img,cv2.MORPH_CLOSE,kernel, iterations = 2)
         return closing
-
+    
     '''
         ASLM Noise Removal
         equalizes the Y channel of an YUV image, Y contains the intensity information
@@ -27,11 +25,11 @@ class Preprocess:
         # convert image to YUV
         img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
         # equalize the histogram of the Y channel
-        img_yuv[:, :, 0] = cv2.equalizeHist(img_yuv[:, :, 0])
+        img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
         # convert image to RGB
         img = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
         return img
-
+    
     '''
         DR noise removal
         apply morphologic close transformation on each channel of RGB image
@@ -77,27 +75,25 @@ class Preprocess:
         # back to RGB
         imgResult = cv2.cvtColor(imgResult, cv2.COLOR_YUV2RGB)
         return imgResult
-
+    
     '''
         apply OTSU threshold
     '''
     @staticmethod
     def OTSUThreshold(img):
         # blur the image
-        blur = cv2.blur(img, (15, 15))
+        blur = cv2.blur(img,(15,15))
         # apply OTSU threshold
-        imgray = cv2.cvtColor(blur, cv2.COLOR_RGB2GRAY)
+        imgray = cv2.cvtColor(blur,cv2.COLOR_RGB2GRAY)
         # remove tint effect
         # if Preprocess.hasTint(img):
         mask = Preprocess.removeTint(img)
         imgray = cv2.add(imgray, mask)
         # # noise removal
-        kernel = np.ones((7, 7), np.uint8)
-        imgray = cv2.morphologyEx(
-            imgray, cv2.MORPH_ERODE, kernel, iterations=2)
+        kernel = np.ones((7,7),np.uint8)
+        imgray = cv2.morphologyEx(imgray,cv2.MORPH_ERODE,kernel, iterations = 2)
         # cv2.imshow('mask',imgray)
-        ret, thresh = cv2.threshold(
-            imgray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+        ret, thresh = cv2.threshold(imgray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
         return ret, thresh
 
     '''
@@ -107,8 +103,7 @@ class Preprocess:
     '''
     @staticmethod
     def SLIC(img):
-        slic = cv2.ximgproc.createSuperpixelSLIC(
-            img, algorithm=cv2.ximgproc.MSLIC, region_size=300, ruler=0.075)
+        slic = cv2.ximgproc.createSuperpixelSLIC(img, algorithm=cv2.ximgproc.MSLIC, region_size=300, ruler=0.075)
         color_img = np.zeros(img.shape, np.uint8)
         color_img[:] = (0, 0, 0)
         for n in range(2):
@@ -129,13 +124,11 @@ class Preprocess:
     @staticmethod
     def KMEANS(img, K=5):
         # apply KMEANS
-        Z = img.reshape((-1, 3))
+        Z = img.reshape((-1,3))
         # convert to np.float32
         Z = np.float32(Z)
-        criteria = (cv2.TERM_CRITERIA_EPS +
-                    cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-        ret, label, center = cv2.kmeans(
-            Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
         center = np.uint8(center)
         res = center[label.flatten()]
         result = res.reshape((img.shape))
@@ -147,23 +140,21 @@ class Preprocess:
     '''
     @staticmethod
     def removeTint(img):
-        H, W = img.shape[:2]
-        mask = np.zeros((H, W), np.uint8)
-        cv2.circle(mask, (W//2, H//2), W//2 + W//50,
-                   (150, 150, 150), -1, cv2.LINE_AA)
-        mask = cv2.blur(mask, (321, 321))
+        H,W = img.shape[:2]
+        mask = np.zeros((H,W), np.uint8)
+        cv2.circle(mask, (W//2, H//2), W//2 + W//50, (150,150,150), -1, cv2.LINE_AA)
+        mask = cv2.blur(mask, (321,321))
         mask = 160 - mask
         return mask
 
     @staticmethod
     def hasTint(img):
-        imgray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        H, W = imgray.shape[:2]
-        mask = np.zeros((H, W), np.uint8)
-        cv2.circle(mask, (W//2, H//2), W//2 + W//50,
-                   (255, 255, 255), -1, cv2.LINE_AA)
-        mask = cv2.blur(mask, (21, 21))
+        imgray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+        H,W = imgray.shape[:2]
+        mask = np.zeros((H,W), np.uint8)
+        cv2.circle(mask, (W//2, H//2), W//2 + W//50, (255,255,255), -1, cv2.LINE_AA)
+        mask = cv2.blur(mask, (21,21))
         # mask = 255 - mask
         mask = cv2.subtract(imgray, mask)
         seuil = 140
-        return mask[10, 10] < seuil
+        return mask[10,10]<seuil
