0819d7b6881b3b4ac3dbb5a4f74cf992dcf582fa
hakimhassani97 <hakimhassani97@gmail.com>
2020-04-25 15:00:12 +0100

    added mezies

---
+++ Menzies.py
@@ -0,0 +1,140 @@
+import cv2
+import numpy as np
+from Diameter import Diameter
+from Data import Data
+from Contours import Contours
+from Color import Color
+from Asymmetry import Asymmetry
+from Preprocess import Preprocess
+from Caracteristics import Caracteristics
+
+class Menzies:
+    '''
+        the Menzies method
+    '''
+    def asymmetry(img, contour):
+        '''
+            returns the asymmetry of the lesion, using homologue method
+        '''
+        asymmetry = Asymmetry.asymmetryIndex(img, contour)
+        return asymmetry
+    
+    def color(img, contour):
+        '''
+            returns the number of colors
+        '''
+        nbColors = Color.colorHSVIntervals(img, contour)
+        return nbColors
+    
+    def darkPoints(img, contour):
+        '''
+            returns the number of dark points, globules, pigmented network, pseudopods in the lesion
+        '''
+        # extract the lesion
+        lesion = Caracteristics.extractLesion(img, contour)
+        # get bounding rect
+        x, y, w, h = cv2.boundingRect(contour)
+        # crop the rect
+        lesion = lesion[y:y + h, x:x + w]
+        # mean color of lesion
+        mean = np.sum(lesion[lesion[:,:,0]>0])
+        mean = mean / np.count_nonzero(lesion[lesion[:,:,0]>0])
+        mean = int(mean * 0.9)
+        # colors intervals
+        blackH = np.array([1, 1, 1], dtype=np.uint8)
+        blackL = np.array([mean, mean, mean], dtype=np.uint8)
+        brownH1 = np.array([13, 81, 47], dtype=np.uint8)
+        brownL1 = np.array([53, 121, 87], dtype=np.uint8)
+        brownH2 = np.array([8 ,34, 151], dtype=np.uint8)
+        brownL2 = np.array([48, 74, 191], dtype=np.uint8)
+        brownH3 = np.array([25 ,21, 70], dtype=np.uint8)
+        brownL3 = np.array([65, 61, 110], dtype=np.uint8)
+        brownH4 = np.array([24 ,25, 102], dtype=np.uint8)
+        brownL4 = np.array([64, 65, 152], dtype=np.uint8)
+        intervalsL = [blackH, brownH1, brownH2, brownH3, brownH4]
+        intervalsH = [blackL, brownL1, brownL2, brownL3, brownL4]
+        res = np.zeros(np.shape(lesion)[:2], dtype=np.uint8)
+        for i in range(0, len(intervalsH)):
+            L = intervalsL[i]
+            H = intervalsH[i]
+            mask = cv2.inRange(lesion, L, H)
+            res = res + mask
+        # get contours
+        ret, thresh = cv2.threshold(res, 0, 100, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+        c, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # filter contours
+        def f(cnt):
+            return cv2.arcLength(cnt, closed=True)>10 and cv2.arcLength(cnt, closed=True)<30
+        contours = list(filter(f, contours))
+        # number of positives
+        n = 0
+        # lesion area
+        lesionArea = cv2.contourArea(contour)
+        for cnt in contours:
+            n = n + cv2.contourArea(cnt)
+        n = n / lesionArea * 100
+        n = round(n ,2)
+        return n
+    
+    def blueGrey(img, contour):
+        '''
+            check for blue grey
+        '''
+        # extract the lesion
+        lesion = Caracteristics.extractLesion(img, contour)
+        # lesion area
+        lesionArea = cv2.contourArea(contour)
+        # get bounding rect
+        x, y, w, h = cv2.boundingRect(contour)
+        # crop the rect
+        lesion = lesion[y:y + h, x:x + w]
+        # set color intervals
+        blueGrayH1 = np.array([90, 60, 50], dtype=np.uint8)
+        blueGrayL1 = np.array([130, 100, 90], dtype=np.uint8)
+        blueGrayH2 = np.array([60, 50, 50], dtype=np.uint8)
+        blueGrayL2 = np.array([100, 90, 90], dtype=np.uint8)
+        intervalsL = [blueGrayH1, blueGrayH2]
+        intervalsH = [blueGrayL1, blueGrayL2]
+        res = np.zeros(np.shape(lesion)[:2], dtype=np.uint8)
+        for i in range(0, len(intervalsH)):
+            L = intervalsL[i]
+            H = intervalsH[i]
+            mask = cv2.inRange(lesion, L, H)
+            res = res + mask
+        # blue grey surface area
+        s = np.count_nonzero(mask[mask>0]) / lesionArea * 100
+        s = round(s, 2)
+        return s
+
+if __name__ == '__main__' :
+    '''
+        test program
+    '''
+    TYPE = 'Melanoma'
+    # TYPE = 'Nevus'
+    BDD = 'ISIC'
+    # BDD = 'PH2'
+    BDD_LOCATION = 'D:/HAKIM/MIV M2/PFE/fichiers prof/MIV 96-2019/Application MIV 96-2019/Code/BDD/'
+    DATA = BDD_LOCATION+BDD+'/'+TYPE+'/'
+    files = Data.loadFilesAsArray(DATA)
+    t = []
+    i = 0
+    for file in files:
+        sImg = DATA+file
+        img = cv2.imread(sImg,cv2.IMREAD_COLOR)
+        # get contours
+        contour = Contours.contours2(img)
+        # draw contours
+        # cv2.drawContours(img, contour, -1, (0, 255, 255), 1)
+        # get color
+        color = Menzies.blueGrey(img, contour)
+        print('['+str(i)+'/'+str(len(files))+']',color)
+        t.append(color)
+        i += 1
+        # draw text
+        x, y = 0, np.shape(img)[0]-3
+        cv2.putText(img,'color : '+str(color), (x,y), cv2.FONT_HERSHEY_SIMPLEX, fontScale = 0.5, color = (0, 0, 0), lineType = 1)
+        cv2.imshow('img',img)
+        if cv2.waitKey() == ord('c'):
+            break
+    cv2.destroyAllWindows()
